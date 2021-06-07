0c24af8879021b544299584059da165847bdcd50
Kritikaporwal <85471290+Kritikaporwal@users.noreply.github.com>
2021-06-07 11:33:33 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react" ;
+
+ function Header() {
+   return (
+     <header>
+      <h1>Microsoft windows</h1>
+     </header>
+   );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className = "note">
+      <h1>Introduction to Windows</h1>
+      <p>
+      Microsoft Windows, commonly referred to as Windows, is a group of several proprietary graphical operating system families, all of which are developed and marketed by Microsoft. Each family caters to a certain sector of the computing industry.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
